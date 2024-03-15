05903fb6b31f65a1d9f87f43bb2e77f4c02a310d
Guilherme Araujo Silva <guilherme.silva@PROWAY.TREINA>
2024-03-14 21:50:29 -0300

    Criacao classe Medico

---
+++ projetoHospital/src/br/com/projetoHospital/model/Medico.java
@@ -0,0 +1,72 @@
+package br.com.projetoHospital.model;
+
+import java.util.List;
+
+public class Medico {
+	
+	private int id;
+	private String nome;
+	private String especializade;
+	private boolean isPlantao;
+	private List<Paciente> pacientes;
+	
+	public Medico () {
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEspecializade() {
+		return especializade;
+	}
+
+	public void setEspecializade(String especializade) {
+		this.especializade = especializade;
+	}
+
+	public boolean isPlantao() {
+		return isPlantao;
+	}
+
+	public void setPlantao(boolean isPlantao) {
+		this.isPlantao = isPlantao;
+	}
+
+	public List<Paciente> getPacientes() {
+		return pacientes;
+	}
+
+	public void setPacientes(List<Paciente> pacientes) {
+		this.pacientes = pacientes;
+	}
+	
+	
+	public void cadastrarPaciente (Paciente paciente) {
+		this.pacientes.add(paciente);
+		System.out.println("\nPaciente cadastrado com sucesso!");
+	}
+	
+	public void imprimirRelatorioPacientes () {
+		
+		System.out.println("- Relatório de Pacientes: dr(a) " + this.nome + " -");
+		for (Paciente paciente : this.pacientes) {
+			paciente.exibirDadosPaciente();
+		}
+		
+	}
+
+}
--- projetoHospital/src/br/com/projetoHospital/model/Paciente.java
+++ projetoHospital/src/br/com/projetoHospital/model/Paciente.java
@@ -1,69 +1,83 @@
 package br.com.projetoHospital.model;
 
 public class Paciente {
-	
+
 	// Atributos
 	private int id;
 	private String nome;
 	private int idade;
 	private String sexo;
 	private boolean isInternado;
-	
+
 	// Construtor vazio
-	public Paciente () {
-		
+	public Paciente() {
+
 	}
-	
+
 	// Construtor com parâmetros
-	public Paciente (int id, String nome, int idade, String sexo) {
+	public Paciente(int id, String nome, int idade, String sexo) {
 		this.id = id;
 		this.nome = nome;
 		this.idade = idade;
 		this.sexo = sexo;
 		this.isInternado = false;
 	}
-	
+
 	// Get
-	public int getId () {
+	public int getId() {
 		return this.id;
 	}
-	
-	public String getNome () {
+
+	public String getNome() {
 		return this.nome;
 	}
-	
-	public int getIdade () {
+
+	public int getIdade() {
 		return this.idade;
 	}
-	
-	public String getSexo () {
+
+	public String getSexo() {
 		return this.sexo;
 	}
-	
-	public boolean isInternado () {
+
+	public boolean isInternado() {
 		return this.isInternado;
 	}
-	
+
 	// Set
-	public void setId (int id) {
+	public void setId(int id) {
 		this.id = id;
 	}
-	
-	public void setNome (String nome) {
+
+	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
-	public void setIdade (int idade) {
+
+	public void setIdade(int idade) {
 		this.idade = idade;
 	}
-	
-	public void setSexo (String sexo) {
+
+	public void setSexo(String sexo) {
 		this.sexo = sexo;
 	}
-	
-	public void setIsInternado (boolean isInternado) {
+
+	public void setIsInternado(boolean isInternado) {
 		this.isInternado = isInternado;
 	}
 
-	
+
+	public void exibirDadosPaciente () {
+		System.out.println("-------------------------------------------");
+		System.out.println("Código: " + this.id + " Paciente: " + this.nome);
+		System.out.println("Idade: " + this.idade + " anos.");
+
+		if (this.idade >= 60) {
+			System.out.println("Paciente tem direito à acompanhante!");
+		} else {
+			System.out.println("Paciente não tem direito à acompanhante!");
+		}
+
+		System.out.println("-------------------------------------------");
+	}
+
 }
